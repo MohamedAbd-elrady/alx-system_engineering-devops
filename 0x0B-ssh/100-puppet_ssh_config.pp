19e939e833523f67a7606e253dd87a094bba8ef6
MohamedAbd-elrady <mmohammedabdelrady@gmail.com>
2024-03-25 10:13:26 +0200

    tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,6 @@
-#!usr/bin/env bash
-#use puppet to make changes to my config files
-#this script is to be run on the puppet master
-file { '/etc/ssh/ssh_config':
-        ensure => present,
-content => "
-Include /etc/ssh/ssh_config.d/*.conf
-Host *
-    IdentityFile ~/.ssh/school
-    passwordAuthentication no
-",
-  mode    => '0644',
+# Docs 
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
 }
